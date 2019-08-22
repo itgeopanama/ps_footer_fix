06ae8e1d63a447149a9783779fcc2d4479829aa0
rquintanam <itgeopanama@gmail.com>
2019-08-21 22:43:12 -0500

    Initial commit

---
+++ __manifest__.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "Remove Footer",
+    'summary': """
+        This is a simple module aim to remove the footer from the odoo interface.""",
+    'description': """
+        This is a simple module aim to remove the footer from the odoo interface.
+    """,
+    'author': "John Carter",
+    'images': ['images/main.png'],
+    'license': 'OPL-1',
+    'category': 'Uncategorized',
+    'version': '0.1.1',
+    'depends': ['base'],
+    'data': [
+        'views/views.xml',
+    ],
+    'installable': True,
+
+}
